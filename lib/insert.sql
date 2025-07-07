cb11795813eb82475bf35ef5b9f801302bc3da20
Crispus Nganga <githogoringash91@gmail.com>
2025-07-07 05:30:24 +0300

    used sql to save and retrieve data

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 10, 'M', 'brown', 'calm', 1),
+('Rowdy', 5, 'M', 'black', 'aggressive', 1),
+('Tabitha', 3, 'F', 'white', 'playful', 1),
+('Sergeant Brown', 7, 'M', 'brown', 'serious', 1),
+('Melissa', 4, 'F', 'gray', 'gentle', 1),
+('Grinch', 6, 'M', 'green', 'grumpy', 0),
+('Wendy', 2, 'F', 'blonde', 'curious', 1),
+(NULL, 1, 'M', 'unknown', 'unknown', 0);
